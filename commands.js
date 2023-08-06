d24ceb2b4bb294d02b65a481ba82db723f97af51
ahmedzaheerSys <ahmed.zaheer@systemsltd.com>
2023-08-06 12:49:12 +0500

    Project initialized

---
+++ commands.js
@@ -0,0 +1,5 @@
+// npx madge dist/main.js --circular
+// To check circular dependency and display the result that , how much circular dependencies exists in project
+
+// npx madge dist/main.js --image GraphInspector.png
+// will create the image of Dependencies
