2b1843eedac676012259d83a81b20b0be3876e68
ahmedzaheerSys <ahmed.zaheer@systemsltd.com>
2023-08-06 13:05:27 +0500

    Lazyloading working

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,7 +1,9 @@
+
 import { Injectable,Inject } from '@nestjs/common';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { Coffee } from './entities/coffee.entity';
+import { LazyModuleLoader } from '@nestjs/core';
 
 export const COFFEES_DATA_SOURCE = Symbol("COFFEES_DATA_SOURCE");
 
@@ -12,9 +14,18 @@ export interface CoffeesDataSource {
 
 @Injectable()
 export class CoffeesService {
-  constructor(@Inject(COFFEES_DATA_SOURCE) dataSource : CoffeesDataSource){}
+  constructor(
+    @Inject(COFFEES_DATA_SOURCE) dataSource : CoffeesDataSource,
+    private readonly lazyModuleLoader: LazyModuleLoader
+    ){}
 
-  create(createCoffeeDto: CreateCoffeeDto) {
+  async create(createCoffeeDto: CreateCoffeeDto) {
+    console.time();
+    const rewardsModuleRef = await this.lazyModuleLoader.load(() => import('../rewards/rewards.module').then((m) => m.RewardsModule))
+    const { RewardsService } = await import ('./../rewards/rewards.service');
+    const rewardsService = rewardsModuleRef.get(RewardsService);
+    console.timeEnd();
+    rewardsService.grantTo();
     return 'This action adds a new coffee';
   }
 
--- src/rewards/rewards.service.ts
+++ src/rewards/rewards.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class RewardsService {}
+export class RewardsService {
+    grantTo() {
+        console.log("Hello from the lazy-loaded RewardsService");
+    }
+}
